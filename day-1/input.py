b30f5d61306c05450ae75f519356992951dd3bcb
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-24 23:26:03 -0500

    input excercise

---
+++ day-1/input.py
@@ -0,0 +1,8 @@
+name = input("What's your name?")
+print(f"Hello {name} ")
+
+print("Hello " + input("What's your name?"))
+
+# input excercise: input and print length of string
+name = input("What is your name? ")
+print(len(name))
--- day-1/main.py
+++ day-1/main.py
@@ -1,1 +1,8 @@
 print("hello world!")
+
+# line break 
+print("hello world\nhello world!\nhelloworld!")
+
+# concatenate strings
+print("carla" + " " + "montano")
+
