c6b7392fd8f2dde940f8fdd06486ab60b6d26e06
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-25 00:03:21 -0500

    day one final project

--- day-1/input.py
+++ day-1/input.py
@@ -1,8 +1,18 @@
-name = input("What's your name?")
-print(f"Hello {name} ")
+# name = input("What's your name?")
+# print(f"Hello {name} ")
 
-print("Hello " + input("What's your name?"))
+# print("Hello " + input("What's your name?"))
 
-# input excercise: input and print length of string
-name = input("What is your name? ")
-print(len(name))
+# # input excercise: input and print length of string
+# name = input("What is your name? ")
+# print(len(name))
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
---
+++ day-1/project-1.py
@@ -0,0 +1,15 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Bame Benerator.")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What's the name of the city your grew up in?.\n").capitalize()
+
+#3. Ask the user for the name of a pet.
+pet = input("What's your pet's name?\n").capitalize()
+
+#4. Combine the name of their city and pet and show them their band name.
+print(f"Your band name could be {city} {pet}")
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+
