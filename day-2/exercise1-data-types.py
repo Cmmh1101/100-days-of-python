7c67cbdc49180b3eae8c6731e1a554145d976826
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-27 23:39:34 -0500

    final tip calculator

--- day-2/exercise1-data-types.py
+++ day-2/exercise1-data-types.py
@@ -7,3 +7,15 @@ two_digit_number = input("Type a two digit number: ")
 num1 = int(two_digit_number[0])
 num2 = int(two_digit_number[1])
 print(num1 + num2)
+
+# round 
+
+print(round(8 / 3))
+
+# round
+
+print(round(8 / 3))
+
+#  round specifying number of decimals
+
+print(round(2.9874562, 2))
