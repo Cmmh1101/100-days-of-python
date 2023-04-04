7402c8152250e93a27d2232939c44620629a2ce9
Carla Montano <cm.emprendiendo@gmail.com>
2023-04-03 22:57:37 -0500

    tails heads

--- day-4/randomization.py
+++ day-4/randomization.py
@@ -3,3 +3,13 @@ import random
 # creating an integer between the range 1 and 10
 random_integer = random.randint(1, 20)
 print(random_integer)
+
+# creating a random floating between 0 and 1.0 not including 1.0
+
+random_float = random.random()
+print(random_float)
+
+random_float_5 = random_float * 5
+print(random_float_5)
+
+# multiply random_float by a # to get a floating number between 0.00 and the desired number example 5
