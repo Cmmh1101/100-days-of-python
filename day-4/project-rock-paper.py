187c461136c8dc5e0faad946af252c130cd528af
Carla Montano <cm.emprendiendo@gmail.com>
2023-04-26 22:53:33 -0500

    fix score and error catch

--- day-4/project-rock-paper.py
+++ day-4/project-rock-paper.py
@@ -40,41 +40,42 @@ for i in range(5):
 
     print(f"you: {choices[playUser - 1]} - computer: {choices[comp_play]}")
 
-    if playUser - 1 == 0 and comp_play == 2:
-        user =+ 1
-        print("You won")
-    elif comp_play == 0 and playUser - 1 == 2:
-        computer =+ 1
-        print("You lose")
-    elif playUser - 1 == 1 and comp_play == 0:
-        user =+ 1
-        print("You won")
-    elif comp_play == 1 and playUser - 1 == 0:
-        computer =+ 1
-        print("You lose")
-    elif playUser - 1 == 2 and comp_play == 1:
-        user =+ 1
-        print("You won")
-    elif comp_play == 2 and playUser - 1 == 1:
-        computer =+ 1
-        print("You lose")
-    elif playUser - 1 == comp_play:
-        print("it's a tie")
+    if playUser > 3 or playUser < 1:
+        print("invalid number you lose!")
     else:
-        print("You typed an invalid number, you lose!")
+        if playUser - 1 == 0 and comp_play == 2:
+            user = user + 1
+            print("You won")
+        elif comp_play == 0 and playUser - 1 == 2:
+            computer = computer + 1
+            print("You lose")
+        elif playUser - 1 == 1 and comp_play == 0:
+            user = user + 1
+            print("You won")
+        elif comp_play == 1 and playUser - 1 == 0:
+            computer = computer + 1
+            print("You lose")
+        elif playUser - 1 == 2 and comp_play == 1:
+            user = user + 1
+            print("You won")
+        elif comp_play == 2 and playUser - 1 == 1:
+            computer = computer + 1
+            print("You lose")
+        elif playUser - 1 == comp_play:
+            print("it's a tie")
+        else:
+            print("You typed an invalid number, you lose!")
 
-    print(f"computer: {computer} - You: {user}")
 
+print("Game over")
+if computer > user:
+    print("You lose!")
+elif computer < user:
+    print("You won!!!!")
+else:
+    print("It's a tie")
 
-    print("Game over")
-    if computer > user:
-        print("You lose!")
-    elif computer < user:
-        print("You won!!!!")
-    else:
-        print("It's a tie")
-
-    print(f"computer: {computer} - You: {user}")
+print(f"computer: {computer} - You: {user}")
 
 
 
